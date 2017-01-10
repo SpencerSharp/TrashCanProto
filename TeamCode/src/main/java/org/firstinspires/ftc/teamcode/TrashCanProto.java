a97f70849760768d3918a53dd9062693dd476892
Spencer Sharp <spencersharp@me.com>
2017-01-10 12:45:45 -0600

    initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrashCanProto.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by spencersharp on 1/10/17.
+ */
+public class TrashCanProto extends LinearOpMode
+{
+}
